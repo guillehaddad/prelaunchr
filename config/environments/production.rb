200718dd5a822ddb240974c1f3047588fbed02ad
Henrique Breim <henrique@breim.com.br>
2017-03-04 04:42:09 -0300

    options to deploy in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,14 +10,17 @@ Prelaunchr::Application.configure do
 
   config.eager_load = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.log_level = :error
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.version = '1.0'
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
